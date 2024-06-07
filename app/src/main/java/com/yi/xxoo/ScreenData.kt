aef65cf3574236150877df22671df42a91504b91
刘一凡 <111629788+lyf12376@users.noreply.github.com>
2024-06-07 22:12:43 +0800

    Revert "数据库及部分页面编写"
    This reverts commit ae290afc0d49329d203484c9e22d37f675dcbe9d.

--- app/src/main/java/com/yi/xxoo/ScreenData.kt
+++
@@ -1,8 +0,0 @@
-package com.yi.xxoo
-
-import androidx.compose.ui.unit.dp
-
-object ScreenData {
-    var screenWidthDp = 0.dp
-    var screenHeightDp = 0.dp
-}
--- app/src/main/java/com/yi/xxoo/di/App.kt
+++
@@ -1,9 +0,0 @@
-package com.yi.xxoo.di
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class App :Application(){
-
-}
